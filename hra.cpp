d9ebf3cca285b0fb85e1c2552ffc840dcd3d1030
OliverFulem <oliver.fulem@gmail.com>
2025-04-06 18:01:15 +0200

    Zacatek hry

--- hra.cpp
+++ hra.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
+struct Postava{
+string jmeno;
+int MaxZivoty;
+int zivoty;
+int MaxMana;
+int mana;
+int sila utoku;
+int zlato;
+int uroven;
+int zkusenosti;
+}
 
 int main(){
+    int vstup;
+    cout << "vytejte ve hre" << endl;
+    cout << "Tvim cilem bude projit lesem kde potkas mnoho bytosti, pres ktere se musis dostat do kralovstvi." << endl;
+    cout << "Pokud si chcete zahrát hru tak sticknete 1 pokud ne tak stisknete 0" << endl;
+    cin >> vstup;
 
-    bool pokracovat;
-do{
-    cout << "vytejte ve hre"
-    cout << "Pokud si chcete zahrát hru tak sticknete 1 pokud ne tak stisknete 0"
-    cin >> pokracovat
-}while(pokracovat == 1)
+    if(start != 1) {
+        cout << "Srabe namas odvahu projit strasidlelnym lesem :) tak se snad uvidime brzy.";
+    }
+    Postava urovne[4];
+
+    uroven[0].jmeno
+
+
+
 }
 
